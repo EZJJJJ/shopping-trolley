631d4fd635429ad644fa747d9dfb5d3fa471f839
EZJJJJ <1638623039@qq.com>
2018-06-29 21:19:02 +0800

    finish

--- index.js
+++ index.js
@@ -40,34 +40,116 @@ $(document).ready(
     }
 
 );
-
+var d1 = 0;
+var d2 = 0;
+var d3 = 0;
+var d4 = 0;
+var d5 = 0;
+var d6 = 0;
+var d7 = 0;
+var d8 = 0;
 // 点击按钮 减少数量
-function crement() {
-    var newnum = parseInt($("#num1").val()) - 1;
+function crement(e) {
+    var newnum = parseInt($("#numcart_item" + e).val()) - 1;
     if (newnum >= 1) {
-        $("#num1").val(newnum);
+        $("#numcart_item" + e).val(newnum);
     }
-    itemprice();
+    itemprice(e);
+    zj();
+    zs();
 }
 
 // 点击按钮 增加数量
-function increment() {
-    var newnum = parseInt($("#num1").val()) + 1;
-    $("#num1").val(newnum);
-    itemprice();
+function increment(e) {
+    var newnum = parseInt($("#numcart_item" + e).val()) + 1;
+    $("#numcart_item" + e).val(newnum);
+    itemprice(e);
+    zj();
+    zs();
 }
 
 // 计算小计内容 用单价*数量
-function itemprice() {
-    var danjia = parseFloat($("#itemdanjia1").text().replace("￥", ""));
-    var shuliang = parseInt($("#num1").val());
+function itemprice(e) {
+    var danjia = parseFloat($("#itemdanjia" + e).text().replace("￥", ""));
+    var shuliang = parseInt($("#numcart_item" + e).val());
     var xiaoji = danjia * shuliang;
-    $("#itemxiaoji1").text("￥" + xiaoji.toFixed(2));
+    $("#itemxiaoji" + e).text("￥" + xiaoji.toFixed(2));
+}
+
+//计算总价
+function zj() {
+    var zjg = 0;
+    for (var a = 1; a <= 8; a++) {
+
+        var danjia = parseFloat($("#itemdanjia" + a).text().replace("￥", ""));
+        var shuliang = parseInt($("#numcart_item" + a).val());
+        if (isNaN(danjia) || isNaN(shuliang)) {
+            continue;
+        } else {
+            
+
+            var xiaoji = danjia * shuliang;
+            zjg = zjg + xiaoji;
+            
+        }
+
+    }
+    $("#em").text("￥" + zjg.toFixed(2));
+}
+
+//计算总数
+function zs() {
+    var x = 0;
+    for (var a = 1; a <= 8; a++) {
+        var shuliang = parseInt($("#numcart_item" + a).val());
+        if (isNaN(shuliang)) {
+            continue;
+        } else {
+            x = x + shuliang;
+        }
+    }
+
+    $("#em2").text(x);
 }
 
 // 删除一条商品
 function removeitem(e) {
     $("#cart_item" + e).remove();
+    switch (e) {
+        case 1:
+            d1 = 0;
+            break;
+        case 2:
+            d2 = 0;
+            break;
+        case 3:
+            d3 = 0;
+            break;
+        case 4:
+            d4 = 0;
+            break;
+        case 5:
+            d5 = 0;
+            break;
+        case 6:
+            d6 = 0;
+            break;
+        case 7:
+            d7 = 0;
+            break;
+        case 8:
+            d8 = 0;
+            break;
+        default:
+            ;
+            break;
+    }
+    zj();
+    zs();
+    if(d1+d2+d3+d4+d5+d6+d7+d8==0){
+        $("#div1_3").css("display", "block");
+        $("#cartxs").css("display", "none");
+    }
 }
 
 
@@ -81,50 +163,136 @@ function removeall() {
     removeitem(6)
     removeitem(7)
     removeitem(8)
+    $("#div1_3").css("display", "block");
+    $("#cartxs").css("display", "none");
 }
 
+//删除选中商品
+function xuanz() {
+    for (var i = 1; i <= 8; i++) {
+        if ($("#ch"+i).is(':checked')) {
+            removeitem(i);
+            console.log("if");
+        }
+    }
+    if(d1+d2+d3+d4+d5+d6+d7+d8==0){
+        $("#div1_3").css("display", "block");
+        $("#cartxs").css("display", "none");
+    }
+}
+
+function sdf(goodsid) {
+    var a;
+    switch (goodsid) {
+        case 1:
+            a = d1;
+            break;
+        case 2:
+            a = d2;
+            break;
+        case 3:
+            a = d3;
+            break;
+        case 4:
+            a = d4;
+            break;
+        case 5:
+            a = d5;
+            break;
+        case 6:
+            a = d6;
+            break;
+        case 7:
+            a = d7;
+            break;
+        case 8:
+            a = d8;
+            break;
+        default:
+            ;
+            break;
+    }
+    return a;
+}
 // 添加一条商品到购物车
 function additem(goodsid) {
-    $("#div1_3").css("display", "none");
-    $("#cartxs").css("display", "block");
-    var itemdvid = "#itemdv" + goodsid;
-    var title = $(itemdvid + " .title").text();
-    var price = $(itemdvid + " .price").text();
-    var imgurl = $("#image"+goodsid).css("background-image");
-    var cart_item_id = "cart_item"+goodsid;
-    
-    imgurl = imgurl.split("(")[1].replace(")","");
-    var content = "<div class='cartitem' id="+cart_item_id+">" +
-        "<div class='cartitemselect'>" +
-        "<div class='selectcb'><input type='checkbox'></div>" +
-        "<div class='cartitemimage'><img class='itemimage' src="+imgurl+"></div>" +
-        "</div>" +
-        "<div class='itemshangpin'>" +
-        title +
-        "</div>" +
-        "<div class='itemdanjia' id='itemdanjia1'>"+price+"</div>" +
-        "<div class='itemshuliang'>" +
-        "<input class='crement' type='button' value='-'' onclick='crement()'><input class='num' id='num1' type='text' value='1' disabled='true'><input class='increment' type='button' value='+' onclick='increment()'>" +
-        "</div>" +
-        "<div class='itemxiaoji' id='itemxiaoji1'></div>" +
-        "<div class='caozuo'><a href='javascript:removeitem("+goodsid+");'>删除</a></div>" +
-        "</div>"
-    $("#cartdisplay").append(content);
+    if (sdf(goodsid) == 0) {
+        $("#div1_3").css("display", "none");
+        $("#cartxs").css("display", "block");
+        var itemdvid = "#itemdv" + goodsid;
+        var title = $(itemdvid + " .title").text();
+        var price = $(itemdvid + " .price").text();
+        var imgurl = $("#image" + goodsid).css("background-image");
+        var cart_item_id = "cart_item" + goodsid;
+        switch (goodsid) {
+            case 1:
+                d1++;
+                break;
+            case 2:
+                d2++;
+                break;
+            case 3:
+                d3++;
+                break;
+            case 4:
+                d4++;
+                break;
+            case 5:
+                d5++;
+                break;
+            case 6:
+                d6++;
+                break;
+            case 7:
+                d7++;
+                break;
+            case 8:
+                d8++;
+                break;
+            default:
+                ;
+                break;
+        }
+
+        imgurl = imgurl.split("(")[1].replace(")", "");
+        var content = "<div class='cartitem' id=" + cart_item_id + ">" +
+            "<div class='cartitemselect'>" +
+            "<div class='selectcb' ><input type='checkbox' id='ch"+goodsid+"'></div>" +
+            "<div class='cartitemimage'><img class='itemimage' src=" + imgurl + "></div>" +
+            "</div>" +
+            "<div class='itemshangpin'>" +
+            title +
+            "</div>" +
+            "<div class='itemdanjia' id='itemdanjia" + goodsid + "'>" + price + "</div>" +
+            "<div class='itemshuliang'>" +
+            "<input class='crement' type='button' value='-'' onclick='crement(" + goodsid + ")'><input class='num' id=" + "num" + cart_item_id + " type='text' value='1' disabled='true'><input class='increment' type='button' value='+' onclick='increment(" + goodsid + ")'>" +
+            "</div>" +
+            "<div class='itemxiaoji' id='itemxiaoji" + goodsid + "'></div>" +
+            "<div class='caozuo'><a href='javascript:removeitem(" + goodsid + ");'>删除</a></div>" +
+            "</div>"
+        $("#cartdisplay").append(content);
+    } else {
+        increment(goodsid)
+    }
+    itemprice(goodsid);
+    zj();
+    zs();
 }
+
 
 
 function chitem(goodsid) {
 
-    $("#itemdv" + goodsid+" #favorcart").css("color", "#ffffff");
-    $("#itemdv" + goodsid+" #favorcart").css("background-color", "#e4393c");
-    $("#itemdv" + goodsid+" #favorcart" + " b").css("background-position", "-28px -56px")
+    $("#itemdv" + goodsid + " #favorcart").css("color", "#ffffff");
+    $("#itemdv" + goodsid + " #favorcart").css("background-color", "#e4393c");
+    $("#itemdv" + goodsid + " #favorcart" + " b").css("background-position", "-28px -56px")
 
 }
 
 function ritem(goodsid) {
-    $("#itemdv" + goodsid+" #favorcart").css("color", "#000000");
-    $("#itemdv" + goodsid+" #favorcart").css("background-color", "#ffffff");
-    $("#itemdv" + goodsid+" #favorcart" + " b").css("background-position", "0 -57px");
+    $("#itemdv" + goodsid + " #favorcart").css("color", "#000000");
+    $("#itemdv" + goodsid + " #favorcart").css("background-color", "#ffffff");
+    $("#itemdv" + goodsid + " #favorcart" + " b").css("background-position", "0 -57px");
 }
 
 // function c_hitem(goodsid) {
